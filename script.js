1460a850aafe765249e10d802d95cc0e5dd214d9
jsprTmbr <191758154+jsprTmbr@users.noreply.github.com>
2025-01-25 20:12:10 +0200

    Reworked code for a browser UI. Add style.css
    Most of the script has been reworked for the player to be using 3
    buttons instead of text. playGame function has been removed as the
    browser basically functions as the game now instead. Added HTML for
    functionality and some CSS for better looks. (Yes it's Terminator based)

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /*
-get computer choice
-computer will randomly return rock, paper or scissors
 get human choice
+play a round with human choice and generate cpu choice at the same time
 
 track human score and set it to 0
 track computer score and set it to 0
@@ -10,7 +9,8 @@ round logic:
 take the player and computer choices as arguments
 play a round, increment the round winner's score
 log the winner
-if there have been less than 5 rounds played then begin a new round
+if either party has less than 5 points, the player can play again.
+once someone has 5 points, the humans choices are disabled and a new game button appears
 
 */
 
@@ -20,160 +20,110 @@ let computerScore = 0
 let humanScore = 0
 let roundCounter = 0
 
-// rock || paper || scissor //
+let rockChoice = document.querySelector("#rockBtn"); //when player clicks on "Rock"
+rockChoice.addEventListener('click',(e) => {
+    humanChoice = "rock";
+    playRound(humanChoice, getComputerChoice())
+})
+let paperChoice = document.querySelector("#paperBtn"); //when player clicks on "Paper"
+paperChoice.addEventListener('click',(e) => {
+    humanChoice = "paper";
+    playRound(humanChoice, getComputerChoice())
+})
+let scissorsChoice = document.querySelector("#scissorsBtn"); //when player clicks on "Scissors"
+scissorsChoice.addEventListener('click',(e) => {
+    humanChoice = "scissors";
+    playRound(humanChoice, getComputerChoice())
+})
+
 function getComputerChoice() {
     choice = Math.floor(Math.random()*100)
     // has 33% chance to choose one of the three choices from a range of 0-99 (i think)
 
     if (choice >= 66) { // rng value of 66 or more (66-99)is scissors
-        console.log(choice)
         return "scissors"
     } else if (choice <= 33) { // rng value of 33 or less (0-33) is rock
-        console.log(choice)
         return "rock"
     } else { // and basically everything between is paper (33-66)
-        console.log(choice)
         return "paper"
     }
-    
-} 
+}
 
-// Two flags for correct or incorrect answers
-// If necessary, loops the popup with a different, more specific text
-// Human has a popup with an input field
-// Popup asks human rock paper or scissors
-// Human enter rock or paper or scissors
-// If human answers something else
-// Tell human that is an invalid answer and have them enter the answer again (loops)
-// If the answer is rock paper or scissor
-// The humans choice is the answer
+function playRound(humanChoice, computerChoice) {
 
-function getHumanChoice() {
-    let validAnswer = false; // If the answer is not valid, asks the user again. aka if the answer is NOT rock, paper or scissor
-    let wrongInput = false;  // The first time around, wrong input is false. if the user enters a wrong input, 
-                             // the program tells the user they entered in a wrong input and asks them to answer again
-    let userInput = ""
+    const gameState = document.querySelector("#gameState"); //links to the html elements
+    const score = document.querySelector("#score");
+    const round = document.querySelector("#round")
+    const newGameSpot = document.querySelector("#gameThings")
 
-    // A while loop to ask the human the answer again until 
-    // They choose one of the correct choices
+    // Player choses rock
+    if (humanChoice == "rock") {
+        if (computerChoice == "scissors") { // Computer - Scissors
+            humanScore++ 
+            gameState.textContent = "Rock beats scissors. You win the round. +1 point to You."
+        } else if ( computerChoice == "rock") { // Computer - Rock
+            gameState.textContent = "Both chose rock. Draw."
+        } else { // Computer - Paper
+            computerScore++
+            gameState.textContent = "Rock loses to paper. Computer wins the round. +1 point to Computer"
+        }
+    // Player choses paper
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "scissors") { // Computer - Scissors
+            computerScore++ 
+            gameState.textContent = "Paper loses to scissors. Computer wins the round. +1 point to Computer."
+        } else if ( computerChoice == "rock") { // Computer - Rock
+            humanScore++
+            gameState.textContent = "Paper beats rock. You win the round. +1 point to You."
+        } else { // Computer - Paper
+            gameState.textContent = "Both chose paper. Draw."
+        }
+    // Player choses scissors
+    } else if (humanChoice == "scissors") { 
+        if (computerChoice == "scissors") { // Computer - Scissors
+            gameState.textContent = "Both chose scissors. Draw."
+        } else if ( computerChoice == "rock") { // Computer - Rock
+            computerScore++
+            gameState.textContent = "Scissors loses to rock. Computer wins the round. +1 point to Computer."
+        } else { // Computer - Paper
+            humanScore++
+            gameState.textContent = "Scissors beats paper. You win the round. +1 point to You."
+        }
+    }
 
-    while(!validAnswer) {
-        if (!wrongInput) {
-            userInput = prompt("Rock, paper or scissors");
-        } else {
-            userInput = prompt("Unrecognised answer. Please enter rock, paper or scissors")
+    score.textContent = `Score: ${humanScore} to ${computerScore}`
+
+    if (humanScore == 5) {
+        round.textContent = "You have racked up 5 points. You win"
+        } else if (computerScore == 5) {
+            round.textContent = "Computer has racked up 5 points. You lose"
         }
 
-        // Lowercase the answer for script to make it shorter i think
-        let choice = userInput.toLowerCase();
-
-        if (choice == "rock" || choice == "paper" || choice == "scissors") {
-            validAnswer = true;
-            console.clear();
-            console.log(`You chose ${choice}.`)
-            return choice;
-            
-        } else {
-            console.log("Wrong Input");
-            wrongInput = true;
-        }
+    if (humanScore == 5 || computerScore == 5) { //if either player has won the game, disables game buttons for player and creates a newgame button 
+        rockChoice.disabled = true;
+        paperChoice.disabled = true;
+        scissorsChoice.disabled = true;
+        let newGameButton = document.createElement("button")
+        newGameButton.setAttribute("id", "NewGame")
+        newGameButton.textContent = "New Game"
+        newGameButton.addEventListener('click',(e) => {
+            newGame()
+        })
+        newGameSpot.appendChild(newGameButton)
     }
 }
 
-function playGame() {
-    
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    
-
-    // playRound is triggered
-    // Human is asked to chooses rock, paper or scissor
-    // Computer makes their choise
-    // Compare the choices with if statements
-    // If player wins the round, gives them a point
-    // If the computer wins the round, gives them a point
-    // If it's a draw, no point are given
-    function playRound(humanChoice, computerChoice) {
-        // Player choses rock
-        if (humanChoice == "rock") {
-            if (computerChoice == "scissors") { // Computer - Scissors
-                humanScore++ 
-                console.log("Rock beats scissors. You win the round. +1 point to You.")
-            } else if ( computerChoice == "rock") { // Computer - Rock
-                console.log("Both chose rock. Draw.")
-            } else { // Computer - Paper
-                computerScore++
-                console.log("Rock loses to paper. Computer wins the round. +1 point to Computer")
-            }
-        // Player choses paper
-        } else if (humanChoice == "paper") {
-            if (computerChoice == "scissors") { // Computer - Scissors
-                computerScore++ 
-                console.log("Paper loses to scissors. Computer wins the round. +1 point to Computer.")
-            } else if ( computerChoice == "rock") { // Computer - Rock
-                humanScore++
-                console.log("Paper beats rock. You win the round. +1 point to You.")
-            } else { // Computer - Paper
-                console.log("Both chose paper. Draw.")
-            }
-        // Player choses scissors
-        } else if (humanChoice == "scissors") { 
-            if (computerChoice == "scissors") { // Computer - Scissors
-                console.log("Both chose scissors. Draw.")
-            } else if ( computerChoice == "rock") { // Computer - Rock
-                computerScore++
-                console.log("Scissors loses to rock. Computer wins the round. +1 point to Computer.")
-            } else { // Computer - Paper
-                humanScore++
-                console.log("Scissors beats paper. You win the round. +1 point to You.")
-            }
-        }
-        roundCounter++
-        console.log(`Current round is: ${roundCounter}/5`)
-        console.log(`Score is: ${humanScore} to ${computerScore}`)
-
-        if (roundCounter < 5) {
-            playGame()
-        } else if (roundCounter == 5) {
-            console.log(`Game over! ${winChecker(humanScore, computerScore)}`)
-            console.log(`To play a new game, type newGame().`)
-        }
-    }
-
-    playRound(humanSelection, computerSelection);
-
-    function winChecker(humanScore, computerScore){
-        let winnerScore = 0                                 //
-        if (humanScore > computerScore) {                   //if human has more points than cp
-            winnerScore = humanScore - computerScore        // How many points human wins by
-            if (winnerScore > 1) {                          // If by more than 1
-                return `You win by ${winnerScore} points!`  // tell the player by how many points
-            } else {
-                return `You win by ${winnerScore} point!`   // if by 1 tell the player by x point
-            }
-
-        } else if ( humanScore < computerScore ) {          // If human has less points than cp
-            winnerScore = computerScore - humanScore        // How many points does cp win by
-            if (winnerScore > 1) {                          // If mopre than 1
-                return `You lose by ${winnerScore} points!` // Tell the player by how many points
-            } else {
-                return `You lose by ${winnerScore} point!`  // If by 1 tell the player by x point
-            }
-
-        } else {                                            // If it's a draw
-            return `Draw! ${humanScore} to ${computerScore}`// Tell the player the draw score
-        }
-    }
-}
-
-function newGame() {        // New game function. Resets all stats to default and triggers playGame()
+function newGame() {        // New game function. Resets all stats to default and removes newgame button
     computerChoice = ""
     humanChoice = ""
     computerScore = 0
     humanScore = 0
     roundCounter = 0
-    playGame();
+    score.textContent = "Score: 0 to 0"
+    round.textContent = "First to 5"
+    gameState.textContent = "Ready to start game."
+    rockChoice.disabled = false;
+    paperChoice.disabled = false;
+    scissorsChoice.disabled = false;
+    document.querySelector("#NewGame").remove();
 }
-
-playGame(); // Triggers on browser open/refresh
-
